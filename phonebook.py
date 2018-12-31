3ee49be7de13f97f1794651ee64e0f2fdf99ae16
DearMad <DearMad@protonmail.com>
2018-12-31 20:42:26 +0330

    Update phonebook.py

--- phonebook.py
+++ phonebook.py
@@ -3,9 +3,13 @@
 list = dict()
 inp = input("Enter for start :) ")
 while inp != "exit".lower():
-    inp = input("> ").lower()
+    inp = input("\n> ").lower()
     if inp == "add".lower():
-        get_name = input("Enter the name * > ")
-        get_num = input("Enter the phone number * > ")
-        get_mail = input("Enter the mail > ")
+        get_name = input("\n\tEnter the name * > ")
+        get_num = input("\n\tEnter the phone number * > ")
+        get_mail = input("\n\tEnter the E-mail > ")
         list[get_name] = (get_num, get_mail)
+    else:
+        print("\ncommand not found :-(")
+else:
+    print("\nGood Bye :-)\n")
