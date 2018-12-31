b26e3c6327f7c752a7fd939b1306c69ff4a33ec4
DearMad <DearMad@protonmail.com>
2018-12-31 20:38:31 +0330

    Update phonebook.py

--- phonebook.py
+++ phonebook.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 
 list = dict()
-
-get_num = input("Enter the number * > ")
-get_name = input("\nEnter the name * > ")
-get_mail = input("\nEnter the E-mail > ")
-
-list[get_name] = (get_num, get_mail)
-print(list)
+inp = input("Enter for start :) ")
+while inp != "exit".lower():
+    inp = input("> ").lower()
+    if inp == "add".lower():
+        get_name = input("Enter the name * > ")
+        get_num = input("Enter the phone number * > ")
+        get_mail = input("Enter the mail > ")
+        list[get_name] = (get_num, get_mail)
