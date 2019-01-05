54dabb16b7773b99b741b793b921f0f87d0a68e8
DearMad <DearMad@protonmail.com>
2019-01-05 13:10:33 +0330

    Update phonebook.py

--- phonebook.py
+++ phonebook.py
@@ -1,39 +1,78 @@
 #!/usr/bin/python3
+from sys import argv
 from os import system, chdir, listdir, getenv
 
-system("clear")
-home_dc = getenv("HOME")
-chdir(home_dc)
-ls = listdir()
-if ".Phone_Lists.txt" not in ls:
-    open(".Phone_Lists.txt", "w+").close()
 
-inp = input("Enter for start :) ")
-while inp != "exit".lower():
-    inp = input("\n> ").lower()
-    if inp == "add".lower():
+class PhoneBook:
+    """
+    ABOUT\n
+    \tThe phone book application for linux with GUI interface and web Version.
+    \tAuthor -> Saman Malekian
+    \tGithub -> https://github.com/SamanMalekian
+    \ttelegram ID -> https://t.me/M4lekian\n
+    OPTIONS\n
+    \t-h\t-->\tShow this page
+    \t-g\t-->\tRun app with GUI interface
+    \t-w\t-->\tRun app with WEB version\n
+    COMMANDS\n
+    \tadd\t -->\tAdd new phone number
+    \tshow\t -->\tShow the registered phone numbers
+    \tsearch\t -->\tSearch with name in registered phone numbers
+    """
+
+    def __init__(self, name=0, num=0, email=0):
+        self.name = name
+        self.email = email
+        self.num = num
+
+    def submit(self):
         file = open(".Phone_Lists.txt", "a")
-        get_name = input("\n\tEnter the name * > ")
-        get_num = int(input("\n\tEnter the phone number * > "))
-        get_mail = input("\n\tEnter the E-mail > ")
-        file.write(f"{get_name}\t{get_num}\t{get_mail}\n")
+        file.write(f"{self.name}\t{self.num}\t{self.email}\n")
         file.close()
-    elif inp == "show".lower():
+
+    def show(self):
         file = open(".Phone_Lists.txt")
-        print("name\tphoneNumber\tmail")
+        print("\nname\tphoneNumber\tmail\n")
         for i in file:
             print(i)
-    elif inp == "search".lower():
-        search_val = input("\nEnter the name for search > ")
-        c = len(search_val)
+
+    def search(self, value):
+        c = len(value)
         f = open(".Phone_Lists.txt", "r")
         read = f.readlines()
         for i in read:
-            if i[:c] == search_val:
+            if i[:c] == value:
                 print(i)
-    elif inp == "exit".lower():
-        pass
+
+
+def main():
+    if "-h" in argv:
+        print(PhoneBook.__doc__)
     else:
-        print("\ncommand not found :-(")
-else:
-    print("\nGood Bye :-)\n")
+        system("clear")
+        home_dc = getenv("HOME")
+        chdir(home_dc)
+        ls = listdir()
+        if ".Phone_Lists.txt" not in ls:
+            open(".Phone_Lists.txt", "w+").close()
+        inp = input("Press the enter :)")
+        while inp != "exit":
+            inp = input("\n-> ")
+            inp = inp.lower()
+            if inp == "add":
+                getName = input("\tName -> ")
+                getNum = input("\tPhone Number -> ")
+                getMail = input("\tEmail -> ")
+                submit = PhoneBook(getName, getNum, getMail)
+                submit.submit()
+            elif inp == "show":
+                show = PhoneBook()
+                show.show()
+            elif inp == "search":
+                search_val = input("\nEnter the name for search > ")
+                search = PhoneBook()
+                search.search(search_val)
+
+
+if __name__ == "__main__":
+    main()
