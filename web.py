1553285efeabd0d58bc4c41a112a0f828388c074
DearMad <DearMad@protonmail.com>
2019-01-20 21:52:33 +0330

    Update web.py

--- web.py
+++ web.py
@@ -1,12 +1,19 @@
 #!/usr/bin/python3
-from flask import Flask
+from phonebook import PhoneBook
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return("That's Right :)")
+    if request.method == 'GET':
+        return(render_template('index.html'))
+    elif request.method == 'POST':
+        name, phoneNumber, email = request.form['name'], request.form['phoneNumber'], request.form['email']
+        submit = PhoneBook(name, phoneNumber, email)
+        submit.submit()
+        return('Information was recorded successfully')
 
 
 if __name__ == "__main__":
