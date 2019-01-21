4ba6a49022a691d0bbaf14f3ccc7b997df2bbd43
DearMad <DearMad@protonmail.com>
2019-01-21 23:22:42 +0330

    Update web.py

--- web.py
+++ web.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 from phonebook import PhoneBook
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
@@ -10,10 +10,25 @@ def index():
     if request.method == 'GET':
         return(render_template('index.html'))
     elif request.method == 'POST':
-        name, phoneNumber, email = request.form['name'], request.form['phoneNumber'], request.form['email']
-        submit = PhoneBook(name, phoneNumber, email)
-        submit.submit()
-        return('Information was recorded successfully')
+        if 'submit_btn' in request.form:
+            name, phoneNumber, email = request.form['name'], request.form['phoneNumber'], request.form['email']
+            submit = PhoneBook(name, phoneNumber, email)
+            submit.submit()
+            return('Information was recorded successfully')
+        elif 'search_btn' in request.form:
+            global value
+            value = request.form['search-engin']
+            return(redirect('/search'))
+        else:
+            return()
+
+
+@app.route('/search')
+def search():
+    c = len(value)
+    f = open("static/.Phone_Lists.txt", "r")
+    read = f.readlines()
+    return(render_template('search-page.html', c=c, f=f, read=read, value=value))
 
 
 if __name__ == "__main__":
