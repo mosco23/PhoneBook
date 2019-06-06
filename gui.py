9459ead7f0177a43853fae98076c4d1314b7d226
DearMad <DearMad@protonmail.com>
2019-06-06 22:08:33 +0430

    Update gui.py

--- gui.py
+++ gui.py
@@ -1,86 +1,51 @@
-import sys
-from database import DataBase
-from PyQt5.QtWidgets import QTabWidget, QWidget, QLineEdit, QPushButton, QHBoxLayout, QTextEdit
+import sqlite3
 
 
-class Window(QTabWidget):
-    def __init__(self):
-        super().__init__()
-        self.window()
-
-    def window(self):
-        # Base
-        self.setGeometry(420, 170, 500, 350)
-        self.setWindowTitle("Phone book ")
-        self.setStyleSheet("background-color: #54646E")
-
-        # Create Tabs
-        self.tab1 = QWidget()
-        self.tab2 = QWidget()
-        self.tab3 = QWidget()
-        self.addTab(self.tab1, "Add")
-        self.addTab(self.tab2, "Search")
-        self.addTab(self.tab3, "Show")
-
-        # Add Tab Objects
-        self.getName = QLineEdit(self.tab1)
-        self.getNum = QLineEdit(self.tab1)
-        self.getMail = QLineEdit(self.tab1)
-        self.getName.setPlaceholderText('Name')
-        self.getNum.setPlaceholderText('Phone Number')
-        self.getMail.setPlaceholderText('example@mail.com')
-
-        self.addButton = QPushButton("add", self.tab1)
-        self.addButton.clicked.connect(self.add)
-
-        horizon = QHBoxLayout()
-        horizon.addWidget(self.getName)
-        horizon.addWidget(self.getNum)
-        horizon.addWidget(self.getMail)
-        horizon.addWidget(self.addButton)
-        horizon.addStretch()
-        self.tab1.setLayout(horizon)
-
-        # Search Tab Objects
-        self.search_inp = QLineEdit(self.tab2)
-        self.search_inp.setPlaceholderText("Search Here")
-        self.search_inp.setGeometry(1, 10, 200, 25)
-
-        self.searchButton = QPushButton("Search", self.tab2)
-        self.searchButton.setGeometry(220, 10, 100, 25)
-        self.searchButton.clicked.connect(self.search)
-
-        self.showBox = QTextEdit(self.tab2)
-        self.showBox.setText("ID\tNAME\tPHONENUMBER   EMAIL")
-        self.showBox.setReadOnly(1)
-        self.showBox.setGeometry(2.5, 50, 491, 250)
-
-        # Show Tab Objects
-        self.textBox = QTextEdit(self.tab3)
-        self.textBox.setText("ID NAME PHONENUMBER   EMAIL\n")
-        self.textBox.setReadOnly(1)
-        self.textBox.setGeometry(2.5, 10, 491, 300)
-
-        self.cBottun = QPushButton("Just Click", self.tab3)
-        self.cBottun.move(200, 150)
-        self.cBottun.clicked.connect(self.cShow)
-
-        self.show()
+class DataBase:
+    def __init__(self, **kwargs):
+        self.dbs = kwargs.get('db')
+        self.table = kwargs.get('table')
+        self.rows = kwargs.get('rows')
+        self.values = kwargs.get('values')
+        if self.dbs:
+            self.db = sqlite3.connect(f'{self.dbs}')
+        else:
+            self.db = sqlite3.connect('dbs/db.db')
+        self.connect = self.db.cursor()
 
     def add(self):
-        DataBase(table="USERS", rows="name, number, email", values=f"'{self.getName.text()}',\
-                    '{self.getNum.text()}', '{self.getMail.text()}'").add()
+        self.connect.execute(f"INSERT INTO {self.table}({self.rows}) VALUES({self.values})")
+        self.db.commit()
+
+    def get(self):
+        if self.rows:
+            self.connect.execute(f"SELECT {self.rows} FROM {self.table}")
+            data = self.connect.fetchall()
+        else:
+            self.connect.execute(f"SELECT * FROM {self.table}")
+            data = self.connect.fetchall()
+        return data
 
     def search(self):
-        data = DataBase(table="USERS", values=f"name='{self.search_inp.text()}'").search()
-        for i in data:
-            self.showBox.append(str(i))
+        if self.rows:
+            self.connect.execute(f"SELECT {self.rows} FROM {self.table} WHERE {self.values}")
+            data = self.connect.fetchall()
+        else:
+            self.connect.execute(f"SELECT * FROM {self.table} WHERE {self.values}")
+            data = self.connect.fetchall()
+        return data
 
-    def cShow(self):
-        self.cBottun.hide()
-        data = DataBase(table="USERS").get()
-        for i in data:
-            self.textBox.append(str(i))
+    def replace(self):
+        self.connect.execute(f"REPLACE INTO {self.table}({self.rows}) VALUES({self.values})")
+        self.db.commit()
 
+    def update(self, search=None):
+        if search:
+            self.connect.execute(f"UPDATE {self.table} SET '{self.rows}'='{self.values}' WHERE {search[0]}={search[1]}")
+        else:
+            self.connect.execute(f"UPDATE {self.table} SET '{self.rows}'='{self.values}'")
+        self.db.commit()
 
-
+    def delete(self):
+        self.connect.execute(f"DELETE FROM {self.table} WHERE {self.rows}={self.values}")
+        self.db.commit()
